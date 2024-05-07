96e4cf27f888e2fd737cd17f185733371c3d125f
Ramiro Lopez <ramiro27.rml@gmail.com>
2024-05-06 21:28:53 -0300

    primer entrega

---
+++ js/script.js
@@ -0,0 +1,57 @@
+/*
+let nombre = prompt("como se llama?");
+let resultado = nombre
+alert ( "saludos" + " " + resultado  )
+
+const tragos = []
+
+function agreagarTrago (nombre,familia,ingrediente){
+    console.log("trago agregado al carrito")
+    const cocktail = {
+        nombre:nombre,
+        ingrediente:ingrediente,
+        familia
+
+    }
+}
+
+function eliminarTrago () {
+    console.log("eliminar trago")
+}
+
+function modificarTrago (){
+    console.log("modificar pedido")
+}
+
+
+function contenido () {
+    console.log("que tiene el trago")
+
+    }
+
+
+
+agreagarTrago(1,"mojito","julep","ron")
+
+let tragoElegido = prompt("¿que quieres tomar hoy?")
+
+alert("solo por hoy descuento en " + julep)
+const julep = 
+let bebida = 5000;
+let ingrediente = 4000; 
+
+
+
+
+if (julep == "lima,menta"){
+    console.log("elegiste un julep, tiene descuento ")
+} else{
+    console.log("no es un julep, no tiene descuento")
+} */
+
+
+
+
+
+
+
